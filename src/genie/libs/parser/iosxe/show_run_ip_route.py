bc376e47478e0f5235a0269839139431eeabd831
Nvhooij <niels.vanhooij@kpn.com>
2021-11-24 16:27:19 +0100

    finished show run | i ^ip route for cisco iosXE

--- src/genie/libs/parser/iosxe/show_run_ip_route.py
+++ src/genie/libs/parser/iosxe/show_run_ip_route.py
@@ -1,6 +1,5 @@
 from genie.metaparser import MetaParser, Any
 import re
-import logging
 
 
 class ShowRunIpRouteSchema(MetaParser):
@@ -8,6 +7,7 @@ class ShowRunIpRouteSchema(MetaParser):
         "ip_routes": {
             Any(): {
                 'vrf': str,
+                'vrf_global': str,
                 'subnet': str,
                 'subnet_mask': str,
                 'next_hop': str,
@@ -15,6 +15,8 @@ class ShowRunIpRouteSchema(MetaParser):
                 'route_name': str,
                 'metric': str,
                 'track_object': str,
+                'permanent': str,
+                'parsing_leftovers': str,
             }
         }
     }
@@ -23,13 +25,29 @@ class ShowRunIpRouteSchema(MetaParser):
 class ShowRunIpRoute(ShowRunIpRouteSchema):
     cli_command = "show running-config | i ^ip route"
 
+    # SEVERAL DIFFERENT REAL WORLD EXAMPLES
     """
-    ip route 192.168.29.0 255.255.255.0 213.162.171.193 track 101
-    ip route 0.0.0.0 0.0.0.0 213.162.171.193
-    ip route 145.13.71.128 255.255.255.128 213.162.171.193
-    ip route 145.13.76.0 255.255.255.0 213.162.171.193
-    ip route 192.168.28.0 255.255.255.0 213.162.171.193
-    ip route 192.168.28.0 255.255.255.0 Null0 10
+    ip route vrf 3rdParty 139.156.202.96 255.255.255.240 10.194.48.110
+    ip route vrf 3rdParty 139.156.202.96 255.255.255.240 10.194.48.110 name Management
+    ip route 139.156.202.96 255.255.255.240 10.194.48.110
+    ip route 139.156.202.96 255.255.255.251 Vlan100
+    ip route 139.156.202.96 255.255.255.240 Vlan100 10.194.48.110
+    ip route 139.156.202.96 255.255.255.240 GigabitEthernet0/1/1
+    ip route 139.156.202.96 255.255.255.240 GigabitEthernet0/1/1 240
+    ip route 139.156.202.96 255.255.255.240 GigabitEthernet0/1/1 10.194.48.110
+    ip route 139.156.202.96 255.255.255.240 GigabitEthernet0/1/1 10.194.48.110 128
+    ip route 139.156.202.96 255.255.255.240 10.194.48.110 name Management
+    ip route 139.156.202.96 255.255.255.240 10.194.48.110 255
+    ip route 139.156.202.96 255.255.255.240 Vlan100 255
+    ip route 139.156.202.96 255.255.255.240 Vlan100 10.194.48.110 255
+    ip route 139.156.202.96 255.255.255.240 10.194.48.110 track 100
+    ip route 139.156.202.96 255.255.255.240 Vlan100 track 100
+    ip route 139.156.202.96 255.255.255.240 Vlan100 10.194.48.110 track 100
+    ip route 139.156.202.96 255.255.255.240 GigabitEthernet0/1/1 10.194.48.110 track 100
+    ip route 139.156.202.96 255.255.255.240 10.194.48.110 255 track 100
+    ip route 139.156.202.96 255.255.255.240 Vlan100 255 track 100
+    ip route 139.156.202.96 255.255.255.240 Vlan100 10.194.48.110 255 track 100
+    ip route 139.156.202.96 255.255.255.240 GigabitEthernet0/1/1 10.194.48.110 255 track 100
     """
 
     def cli(self, output=None):
@@ -40,36 +58,100 @@ class ShowRunIpRoute(ShowRunIpRouteSchema):
 
         result_dict = {}
 
-        p0 = re.compile(
-            r'((^ip\sroute|^ip\sroute\s(?P<vrf>vrf\s([A-Za-z0-9]*))))\s+(?P<subnet>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+(?P<subnet_mask>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+(?P<next_hop>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|\w+.*?)(\s+((name)\s+(?P<name>.*($|\s))|track\s+(?P<track_object>\d+)|(((?P<forward_address>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))|(?P<metric>\d{0,3})))|\s+(name)\s+(?P<name_two>.*)|\s+track\s+(?P<track_object_two>\d+)|\s+(?P<metric_two>\d+)|)')
-        logging.debug(out)
-        for count, line in enumerate(out.splitlines()):
-            line = line.strip()
+        # MANDATORY EXAMPLE STATEMENT: ip route 139.156.202.96 255.255.255.240 OR ip route 139.156.202.96 255.255.255.240 GigabitEthernet0/1/1 WHERE VRF IS AN OPTIONAL
+        p_ip_route_with_next_hop = re.compile(r'((^ip\sroute|^ip\sroute\svrf\s(?P<vrf>([A-Za-z0-9-_]*))))\s+(?P<subnet>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+(?P<subnet_mask>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+(?P<next_hop>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|[A-Za-z0-9-_:\.\/]*)')
+        # FINDS OPTIONAL track tracking_object: ex track 100
+        p_track_with_track_object = re.compile(r'(.*)track\s+(?P<tracking_object>\d+)')
+        # FINDS OPTIONAL name iamastaticroute: ex name Management
+        p_route_name = re.compile(r'(.*)name\s+(?P<route_name>([A-Za-z0-9-_@\"\`\&\,\+\=\/\'\.\(\)\[\]]*))')
+        p_forward_address = re.compile(r'(?P<forward_address>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
+        p_metric = re.compile(r'(?P<metric>\d+)')
+        p_permanent = re.compile(r'(?P<permanent>permanent)')
+        p_vrf_global = re.compile(r'(?P<vrf_global>global)')
 
-            match = p0.match(line)
-            if match:
+        for count, line in enumerate(out.splitlines()):
+            try:
+                count = count+1
+                line = line.strip()
+                temp_string = line
+
+                match_route_with_next_hop = p_ip_route_with_next_hop.match(line)
+                vrf = ''
+                if match_route_with_next_hop:
+                    vrf = match_route_with_next_hop.groupdict()['vrf'] or ''
+                    subnet = match_route_with_next_hop.groupdict()['subnet']
+                    subnet_mask = match_route_with_next_hop.groupdict()['subnet_mask']
+                    next_hop = match_route_with_next_hop.groupdict()['next_hop']
+                    # we remove the default statements from the result to parse the chaos after it.
+                    temp_string = temp_string.replace(match_route_with_next_hop.group(), '').strip()
+
+                route_name = ''
+                match_naming_statements = p_route_name.match(temp_string)
+                if match_naming_statements:
+                    route_name = match_naming_statements.groupdict()['route_name']
+                    # we remove name statements from the line
+                    temp_string = temp_string.replace(f'name {route_name}', '').strip()
+
+                track_object = ''
+                match_tracking_statements = p_track_with_track_object.match(temp_string)
+                if match_tracking_statements:
+                    track_object = match_tracking_statements.groupdict()['tracking_object']
+                    # we remove track statements from the line
+                    temp_string = temp_string.replace(f'track {track_object}', '').strip()
+
+                # FORWARD ADRESS IS NOT PROPERLY MARKED BY CISCO. NO PREPEND INLINE
+                forward_address = ''
+                match_forward_address = p_forward_address.match(temp_string)
+                if match_forward_address:
+                    forward_address = match_forward_address.groupdict()['forward_address']
+                    # we remove forward_address statements from the line
+                    temp_string = temp_string.replace(f'{forward_address}', '').strip()
+
+                # METRIC IS NOT PROPERLY MARKED BY CISCO. NO PREPEND INLINE
+                metric = ''
+                match_metric = p_metric.match(temp_string)
+                if match_metric:
+                    metric = match_metric.groupdict()['metric']
+                    # we remove metric statements from the line
+                    temp_string = temp_string.replace(f'{metric}', '').strip()
+
+                permanent = ''
+                match_permanent = p_permanent.match(temp_string)
+                if match_permanent:
+                    permanent = match_permanent.groupdict()['permanent']
+                    # we remove metric statements from the line
+                    temp_string = temp_string.replace(f'{permanent}', '').strip()
+
+                vrf_global = ''
+                match_permanent = p_vrf_global.match(temp_string)
+                if match_permanent:
+                    vrf_global = match_permanent.groupdict()['vrf_global']
+                    # we remove metric statements from the line
+                    temp_string = temp_string.replace(f'{vrf_global}', '').strip()
+
                 if 'ip_routes' not in ip_routes_dict:
                     result_dict = ip_routes_dict.setdefault('ip_routes', {})
-                vrf = match.groupdict()['vrf'] or ''
-                subnet = match.groupdict()['subnet']
-                subnet_mask = match.groupdict()['subnet_mask']
-                next_hop = match.groupdict()['next_hop']
-                forward_address = match.groupdict()['forward_address'] or ''
-                track_object = match.groupdict()['track_object'] or \
-                               match.groupdict()['track_object_two'] or ''
-                metric = match.groupdict()['metric'] or \
-                         match.groupdict()['metric_two'] or ''
-                name = match.groupdict()['name'] or \
-                       match.groupdict()['name_two'] or ''
+
+                parsing_errors = ''
+                if temp_string:
+                    parsing_errors = temp_string
+
                 result_dict[count] = {
                     'vrf': vrf,
+                    'vrf_global': vrf_global,
                     'subnet': subnet,
                     'subnet_mask': subnet_mask,
                     'next_hop': next_hop,
                     'forward_address': forward_address,
-                    'route_name': name,
+                    'route_name': route_name,
                     'metric': metric,
-                    'track_object': track_object
+                    'track_object': track_object,
+                    'permanent': permanent,
+                    'parsing_leftovers': parsing_errors
                 }
+                continue
+
+            except UnboundLocalError as e:
+                print(line) # have to make something sustainable
                 continue
         return ip_routes_dict
