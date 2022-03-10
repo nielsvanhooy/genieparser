41362c8485402d29b4f27f84e1f0aa671f993b5e
Nvhooij <niels.vanhooij@kpn.com>
2022-03-10 19:34:31 +0100

    .

--- src/genie/libs/parser/iosxe/show_run.py
+++ src/genie/libs/parser/iosxe/show_run.py
@@ -972,7 +972,7 @@ class ShowRunInterface(ShowRunInterfaceSchema):
             if m:
                 group = m.groupdict()
                 intf_dict.update(
-                    {'negotiation_auto': False if group['negotiation'] else True})
+                    {'negotiation_auto': group['negotiation'] is None})
                 continue
 
             # cdp enable
